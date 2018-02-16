a9734cd12e51d94c49cc3c4761e7796c5b1941d4
Daniel Zurawski <djzurawski@gmail.com>
2018-02-15 23:02:51 -0700

    part 6

--- hash_server.go
+++ hash_server.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"crypto/sha512"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"path"
@@ -15,6 +16,12 @@ import (
 
 var id_cnt uint64
 var hashes sync.Map
+var total_hash_time uint64
+
+type Stats struct {
+	Total   uint64  `json:"total"`
+	Average float64 `json:"average"`
+}
 
 func hashString(str string) string {
 
@@ -27,7 +34,7 @@ func hashString(str string) string {
 
 func save_hash(id uint64, password string) {
 
-	time.Sleep(5 * time.Second)
+	time.Sleep(4 * time.Second)
 	hashed_pass := hashString(password)
 	hashes.Store(id, hashed_pass)
 	fmt.Println("hashed")
@@ -35,12 +42,16 @@ func save_hash(id uint64, password string) {
 
 func hashHandler(resp http.ResponseWriter, req *http.Request) {
 
+	start := time.Now()
 	req.ParseForm()
 	args := req.Form
 	password := args["password"][0]
 
 	id := atomic.AddUint64(&id_cnt, 1)
 	go save_hash(id, password)
+	elapsed := time.Since(start)
+	atomic.AddUint64(&total_hash_time, uint64(elapsed))
+
 	fmt.Fprintf(resp, "%d\n", id)
 }
 
@@ -49,7 +60,6 @@ func retrieveHandler(resp http.ResponseWriter, req *http.Request) {
 	id, _ := strconv.ParseUint(path.Base(uri), 10, 64)
 	hash, _ := hashes.Load(id)
 	fmt.Fprintf(resp, "%s\n", hash)
-
 }
 
 func shutdownHandler(shutdown chan<- bool) func(resp http.ResponseWriter, req *http.Request) {
@@ -60,6 +70,22 @@ func shutdownHandler(shutdown chan<- bool) func(resp http.ResponseWriter, req *h
 	}
 }
 
+func statsHandler(resp http.ResponseWriter, req *http.Request) {
+
+	total_hashes := atomic.LoadUint64(&id_cnt)
+	hash_time := atomic.LoadUint64(&total_hash_time)
+
+	var avg_hash_time float64
+	if total_hashes == 0 {
+		avg_hash_time = 0.0
+	} else {
+		avg_hash_time = (float64(hash_time) / float64(total_hashes)) / float64(time.Millisecond)
+	}
+
+	stat := Stats{Total: total_hashes, Average: avg_hash_time}
+	json.NewEncoder(resp).Encode(stat)
+}
+
 func main() {
 
 	srv := http.Server{Addr: ":8080"}
@@ -68,6 +94,7 @@ func main() {
 	go func() {
 		http.HandleFunc("/hash/", retrieveHandler)
 		http.HandleFunc("/hash", hashHandler)
+		http.HandleFunc("/stats", statsHandler)
 		http.HandleFunc("/shutdown", shutdownHandler(shutdown))
 		srv.ListenAndServe()
 	}()
